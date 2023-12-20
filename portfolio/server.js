348f632459fbd37bce7b038450ea5db1a5785d20
drnxv <87783549+drnxv@users.noreply.github.com>
2023-12-20 14:31:16 -0500

    started home page

--- portfolio/server.js
+++ portfolio/server.js
@@ -1,37 +1,26 @@
 // imports & initial setup
-import bodyParser from 'body-parser';
-import express from 'express';
-import path from 'path';
-import favicon from 'serve-favicon';
+const bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
 
 const app = express();
 
 // process/terminal control
 process.stdin.setEncoding('utf-8');
 const port = process.argv[2];
-console.log(`Web server started and running at http://localhost:${port}/`);
-const prompt = 'Stop to shutdown the server: ';
-
-process.stdout.write(prompt);
-process.stdin.on('readable', function () {
-  let input = process.stdin.read();
-  if (input !== null) {
-    let cmd = input.trim();
-    if (cmd === 'stop') {
-      process.stdout.write('Shutting down the server\n');
-      process.exit(0);
-    } else {
-      process.stdout.write(`Invalid command: ${cmd}\n`);
-    }
-
-    process.stdout.write(prompt);
-    process.stdin.resume();
-  }
-});
 
 // setting directory of ejs pages
 app.set('views', path.resolve(__dirname, 'templates'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('public'));
 
 // rendering pages
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
