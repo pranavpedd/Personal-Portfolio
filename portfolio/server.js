81e392cf08186f537fad23832895217bd09d06c8
drnxv <87783549+drnxv@users.noreply.github.com>
2023-12-21 19:29:25 -0500

    working on about me page - need to get image working

--- portfolio/server.js
+++ portfolio/server.js
@@ -13,7 +13,7 @@ const port = process.argv[2];
 // setting directory of ejs pages
 app.set('views', path.resolve(__dirname, 'templates'));
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static('public'));
 app.use(favicon(path.join(__dirname, 'public/images', 'golden p.png')));
 
